6689de3a487f16cb02261d28f562cbb94e6e3872
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-05-22 23:38:21 +0700

    fix: current turn concept

--- src/main/java/models/GameManager.java
+++ src/main/java/models/GameManager.java
@@ -1,5 +1,9 @@
 package models;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Path;
@@ -7,20 +11,38 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import gamexception.GameException;
+import utils.Pair;
+
 public class GameManager {
-    private int curPlayer;
+    private static GameManager instance;
     private int curTurn;
     private Player player1;
     private Player player2;
     private List<FileLoader> listFileLoader;
     private final String interfaceName = "FileLoader";
 
-    public GameManager(Player p1, Player p2){
+    private GameManager(){
         curTurn = 1;
-        curPlayer = 1;
-        player1 = p1;
-        player2 = p2;
+        player1 = new Player();
+        player2 = new Player();
         listFileLoader = new ArrayList<>();
+    }
+
+    public static GameManager getInstance(){
+        if(instance==null){
+            instance = new GameManager();
+        }
+        return instance;
+    }
+
+    public FileLoader getFileLoader(String name){
+        for(FileLoader fileLoader: listFileLoader){
+            if(fileLoader.getClass().getName().equals(name)){
+                return fileLoader;
+            }
+        }
+        return null;
     }
 
     public Player getPlayer1(){
@@ -32,29 +54,25 @@ public class GameManager {
     }
 
     public Player getCurPlayer(){
-        if(curPlayer==1){
+        if(curTurn%2==1){
             return player1;
         }
         return player2;
     }
 
     public Player getEnemyPlayer(){
-        if(curPlayer==1){
-            return player2;
+        if(curTurn%2==0){
+            return player1;
         }
-        return player1;
+        return player2;
+    }
+
+    public int getCurTurn(){
+        return curTurn;
     }
 
     public void setCurTurn(int curTurn){
         this.curTurn = curTurn;
-    }
-
-    public void updateCurPlayer(){
-        if(curPlayer==1){
-            curPlayer++;
-        }else{
-            curPlayer = 1;
-        }
     }
 
     public void agePlants(List<LivingThing> field){
@@ -66,21 +84,17 @@ public class GameManager {
     }
 
     public Player next(){
-        if(curPlayer==2){
-            curTurn++;
-        }
-
-        if(curTurn==10){
+        if(curTurn==40){
             return null;
         }
+        curTurn++;
 
         agePlants(player1.getField());
         agePlants(player2.getField());
-        updateCurPlayer();
         return getCurPlayer();    
     }
 
-    public void addPlugin(String jarDir){
+    public void addPlugin(String jarDir) throws Exception{
         Path path = Paths.get(jarDir);
         String fileName = path.getFileName().toString();
         String className = fileName.substring(0,fileName.lastIndexOf('.'));
@@ -93,20 +107,52 @@ public class GameManager {
                     throw new Exception("Plugin Loader tidak mengimplementasi Interface FileLoader!");
                 }
                 listFileLoader.add((FileLoader)loadedClass.getDeclaredConstructor().newInstance());
+            }catch (ClassNotFoundException e) {
+                throw new Exception("Class tidak ditemukan di jar file.");
+            } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                throw new Exception("Gagal menginstansiasi class dari JAR file.");
+            } catch (Exception e) {
+                throw new Exception("File yang dipilih tidak berekstensi .jar atau terjadi kesalahan lainnya.");
             }
-        }catch(Exception e){
-            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            throw new Exception("Invalid JAR file URL.");
+        } catch (FileNotFoundException e) {
+            throw new Exception("JAR file not found.");
+        } catch (Exception e) {
+            throw new Exception("An unexpected error occurred.");
         }
     }
 
+    public static void main(String[] args) {
+        GameManager game = GameManager.getInstance();
+        // Shop shop = Shop.getInstance();
+        // shop.addItem("SIRIP_HIU", 10);
+        // shop.addItem("SUSU", 5);
+
+        // Player p = game.getCurPlayer();
+        // Product product = (Product)GameContext.createObject("SUSU");
+        // Item item = (Item)GameContext.createObject("ACCELERATE");
+        // Carnivore hiu = (Carnivore)GameContext.createObject("HIU_DARAT");
+        // item.useEffect(hiu);
+        // try {
+        //     p.addCardInDeck(product, 1);
+        //     p.addCardInField(hiu, new Pair<Integer,Integer>(0, 2));
+        // } catch (GameException e) {
+        //     e.printStackTrace();
+        // }
+        // System.out.println(hiu.getItems().size());
+        // FileLoader loader = game.getFileLoader("models.TxtFileLoader");
+        // try {
+        //     loader.save("src/main/java/models/gamestate");
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
+        FileLoader loader = game.getFileLoader("models.TxtFileLoader");
+        try {
+            loader.load("src/main/java/models/gamestate/");
+            loader.save("src/main/java/models/tes");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
-/*
- * 1. Minta field sndiri sm lawan
- * 2. minta active deck
- * 3. swap element
- * 4. harvest
- * 5. update barang shop
- * 6. beli, barang lgsg di active deck
- * 7. keranjang di shop
- * 8. Shuffle, cek dek
- */
