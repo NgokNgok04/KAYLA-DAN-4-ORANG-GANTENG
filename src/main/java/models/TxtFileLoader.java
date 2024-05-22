8239855c2c565a5d254654b2e61ad2c070052e8d
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-05-22 23:37:40 +0700

    feat: add TxtFileLoader(work)

--- src/main/java/models/TxtFileLoader.java
+++ src/main/java/models/TxtFileLoader.java
@@ -1,9 +1,12 @@
 package models;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -33,7 +36,8 @@ public class TxtFileLoader implements FileLoader{
         }
     }
 
-    public void loadGameState(GameManager game, Scanner reader){
+    public void loadGameState(Scanner reader){
+        GameManager game = GameManager.getInstance();
         game.setCurTurn(Integer.parseInt(reader.nextLine()));
         Shop shop = Shop.getInstance();
 
@@ -54,8 +58,9 @@ public class TxtFileLoader implements FileLoader{
         for(int i=0;i<nActive;i++){
             String[] parsedLine = reader.nextLine().split(" ");
             try{
+                int idx = Pair.convertTokenToPair(parsedLine[0]).getFirst();
                 GameObject obj = GameContext.createObject(parsedLine[1]);
-                player.addCardInDeck(obj, i);
+                player.addCardInDeck(obj, idx);
             }catch(GameException e){
                 e.printMessage();
             }
@@ -64,7 +69,7 @@ public class TxtFileLoader implements FileLoader{
         int nField = Integer.parseInt(reader.nextLine());
         for(int i=0;i<nField;i++){
             String[] parsedLine = reader.nextLine().split(" ");
-            int idx = Pair.convertTokenToPair(parsedLine[0]).convertPairToIdx();
+            Pair<Integer,Integer> pos = Pair.convertTokenToPair(parsedLine[0]);
             
             GameObject obj = GameContext.createObject(parsedLine[1]);
             LivingThing livingObj = (LivingThing)obj;
@@ -78,30 +83,119 @@ public class TxtFileLoader implements FileLoader{
 
             int nItems = Integer.parseInt(parsedLine[3]);
             for(int j=0;j<nItems;j++){
-                Item item = (Item) GameContext.createObject(parsedLine[j+3]);
-                livingObj.addItem(item);
+                Item item = (Item) GameContext.createObject(parsedLine[j+4]);
+                if(item instanceof Accelerate || item instanceof Delay){
+                    livingObj.addItem(item);
+                }else{
+                    item.useEffect(livingObj);
+                }
             }
+            player.addCardInField(livingObj, pos);
         }
-
     }
 
-    public void load(String dirpath,GameManager game) throws Exception{
+    public void load(String dirpath) throws Exception{
+        GameManager game = GameManager.getInstance();
         if(!isValid(dirpath)){
             throw new Exception("State Files Invalid");
         }
-        File fileObj = new File(dirpath);
-        try (Scanner reader = new Scanner(fileObj,"gamestate.txt")) {
-            loadGameState(game,reader);
+        String filePathState = dirpath+File.separator+"gamestate.txt";
+        String filePathP1 = dirpath+File.separator+"player1.txt";
+        String filePathP2 = dirpath+File.separator+"player2.txt";
+
+        try (Scanner reader = new Scanner(new File(filePathState))) {
+            loadGameState(reader);
         }
-        try (Scanner reader = new Scanner(fileObj,"player1.txt")) {
+        try (Scanner reader = new Scanner(new File(filePathP1))) {
             loadPlayer(game.getPlayer1(),reader);
         }
-        try (Scanner reader = new Scanner(fileObj,"gamestate.txt")) {
+        try (Scanner reader = new Scanner(new File(filePathP2))) {
             loadPlayer(game.getPlayer2(),reader);
         }
     }
 
-    public void save(String dirpath,GameManager game){
-        
+    public void saveGameState(String dirpath){
+        GameManager game = GameManager.getInstance();
+        Path filePath = Paths.get(dirpath, "gamestate.txt");
+        File obj = filePath.toFile();
+        Shop shop = Shop.getInstance();
+        try{
+            obj.createNewFile();
+            try(FileWriter writer = new FileWriter(obj)){
+                writer.write(game.getCurTurn()+"\n");
+
+                int nItems = shop.getCountItemsReady();
+                ArrayList<Pair<String,Integer>> items = shop.getItemsReady();
+
+                writer.write(nItems+"\n");
+                for(int i=0;i<nItems;i++){
+                    writer.write(items.get(i).getFirst()+" "+items.get(i).getSecond()+"\n");
+                }
+            };
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void savePlayer(String dirpath,Player player,int idx){
+        Path filePath = Paths.get(dirpath, "player"+idx+".txt");
+        File obj = filePath.toFile();
+        try{
+            obj.createNewFile();
+            try(FileWriter writer = new FileWriter(obj)){
+                writer.write(player.getMoney()+"\n");
+                writer.write(player.getDeckSlot()+"\n");
+
+                List<GameObject> deck = player.getActiveDeck();
+                int countDeckSize = deck.size();
+                int countActive = player.getCountActiveCard();
+
+                writer.write(countActive+"\n");
+                for(int i=0;i<countDeckSize;i++){
+                    if(!deck.get(i).isActive()){
+                        continue;
+                    }
+                    writer.write("A0"+(i+1)+" ");
+                    writer.write(deck.get(i).getName()+"\n");
+                } 
+
+                int countActiveField = player.getCountActiveField();
+                List<LivingThing> field = player.getField();
+                int countFieldSize = field.size();
+                
+                writer.write(countActiveField+"\n");
+                for(int i=0;i<countFieldSize;i++){
+                    if(!player.getField().get(i).isActive()){
+                        continue;
+                    }
+                    writer.write(Pair.convertPairToToken(Pair.convertIdxToPair(idx))+" ");
+                    writer.write(field.get(i).getName()+" ");
+                    if(field.get(i) instanceof Animal animal){
+                        writer.write(animal.getWeight()+" ");
+                    }else{
+                        Plant plant = (Plant)field.get(i);
+                        writer.write(plant.getAge()+" ");
+                    }
+
+                    List<Item> items = field.get(i).getItems();
+                    int countItems = items.size();
+                    writer.write(countItems+" ");
+
+                    for(int j=0;j<countItems;j++){
+                        writer.write(items.get(j).getName()+" ");
+                    }
+                    writer.write("\n");
+                }
+            };
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void save(String dirpath){
+        GameManager game = GameManager.getInstance();
+        saveGameState(dirpath);
+        savePlayer(dirpath, game.getPlayer1(),1);
+        savePlayer(dirpath, game.getPlayer2(), 2);
     }
 }
